48508ea19f489bd80df6a795f50f10d7c154c966
Dayannis-PC\Dayannis <dayannis.y@gmail.com>
2018-07-23 11:13:17 -0400

    Logito

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
 <!--perfil menu -->
 <div class="profile clearfix">
   <div class="profile_pic">
-    <img src="{{asset('img/logo.jpg')}}" alt="..." class="img-circle profile_img">
+    <img src="{{asset('img/logo.png')}}" alt="..." class="img-circle profile_img">
   </div>
   <div class="profile_info">
     <span>Bienvenido,</span>
